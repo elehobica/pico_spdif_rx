d3f9e33233b533934672d5934b2864fa6ce0b957
elehobica <elehobica@gmail.com>
2023-01-27 23:33:16 +0900

    revise cycle to meet with 125 MHz system clock

--- samples/detect_samp_freq/main.cpp
+++ samples/detect_samp_freq/main.cpp
@@ -26,16 +26,16 @@ static inline uint32_t _millis(void)
 	return to_ms_since_boot(get_absolute_time());
 }
 
-void pw_set_pll_usb_96MHz()
+void set_pll_usb_96MHz()
 {
-    // Set PLL_USB 96MHz
+    // Set PLL_USB 96.0 MHz
     pll_init(pll_usb, 1, 1536 * MHZ, 4, 4);
     clock_configure(clk_usb,
         0,
         CLOCKS_CLK_USB_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
         96 * MHZ,
         48 * MHZ);
-    // Change clk_sys to be 96MHz.
+    // Change clk_sys to be 96.0 MHz.
     clock_configure(clk_sys,
         CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
         CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
@@ -52,13 +52,37 @@ void pw_set_pll_usb_96MHz()
     stdio_usb_init(); // don't call multiple times without stdio_usb_deinit because of duplicated IRQ calls
 }
 
+void measure_freqs(void) {
+    uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+    uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+    uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+    uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+    uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+    uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+    uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+    uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+
+    printf("pll_sys  = %dkHz\n", f_pll_sys);
+    printf("pll_usb  = %dkHz\n", f_pll_usb);
+    printf("rosc     = %dkHz\n", f_rosc);
+    printf("clk_sys  = %dkHz\n", f_clk_sys);
+    printf("clk_peri = %dkHz\n", f_clk_peri);
+    printf("clk_usb  = %dkHz\n", f_clk_usb);
+    printf("clk_adc  = %dkHz\n", f_clk_adc);
+    printf("clk_rtc  = %dkHz\n", f_clk_rtc);
+
+    // Can't measure clk_ref / xosc as it is the ref
+}
+
 int main()
 {
-    // Call stdio_usb_init() in pw_set_pll_usb_96MHz() for modified code rather than stdio_init_all()
-    //stdio_init_all();
+    // Call stdio_usb_init() in set_pll_usb_96MHz() for modified code rather than stdio_init_all()
+    stdio_init_all();
 
     // Set PLL_USB 96MHz and use it for PIO clock for I2S
-    pw_set_pll_usb_96MHz();
+    //set_pll_usb_96MHz();
+
+    measure_freqs();
 
     // DCDC PSM control
     // 0: PFM mode (best efficiency)
