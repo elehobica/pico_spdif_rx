76441049356174db64b4ad5b218beef1e7090487
elehobica <elehobica@gmail.com>
2023-01-28 20:39:15 +0900

    clean up detect_samp_freq/main.cpp

--- samples/detect_samp_freq/main.cpp
+++ samples/detect_samp_freq/main.cpp
@@ -5,52 +5,12 @@
 /------------------------------------------------------*/
 
 #include <stdio.h>
-#include <math.h>
-#include <cinttypes>
-
 #include "pico/stdlib.h"
-#include "pico/stdio_usb.h"
-
-#include "hardware/pll.h"
 #include "hardware/clocks.h"
-
 #include "spdif_rx/spdif_rx.h"
-
-#define SAMPLES_PER_BUFFER 1156 // Samples / channel
 
 static constexpr uint8_t PIN_DCDC_PSM_CTRL = 23;
 static constexpr uint8_t PIN_PICO_SPDIF_RX_DATA = 15;
-
-static inline uint32_t _millis(void)
-{
-	return to_ms_since_boot(get_absolute_time());
-}
-
-void set_pll_usb_96MHz()
-{
-    // Set PLL_USB 96.0 MHz
-    pll_init(pll_usb, 1, 1536 * MHZ, 4, 4);
-    clock_configure(clk_usb,
-        0,
-        CLOCKS_CLK_USB_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
-        96 * MHZ,
-        48 * MHZ);
-    // Change clk_sys to be 96.0 MHz.
-    clock_configure(clk_sys,
-        CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
-        CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
-        96 * MHZ,
-        96 * MHZ);
-    // CLK peri is clocked from clk_sys so need to change clk_peri's freq
-    clock_configure(clk_peri,
-        0,
-        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS,
-        96 * MHZ,
-        96 * MHZ);
-    // Reinit uart/usb_cdc now that clk_peri has changed
-    stdio_uart_init();
-    stdio_usb_init(); // don't call multiple times without stdio_usb_deinit because of duplicated IRQ calls
-}
 
 void measure_freqs(void) {
     uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
@@ -76,13 +36,9 @@ void measure_freqs(void) {
 
 int main()
 {
-    // Call stdio_usb_init() in set_pll_usb_96MHz() for modified code rather than stdio_init_all()
     stdio_init_all();
 
-    // Set PLL_USB 96MHz and use it for PIO clock for I2S
-    //set_pll_usb_96MHz();
-
-    measure_freqs();
+    //measure_freqs();
 
     // DCDC PSM control
     // 0: PFM mode (best efficiency)
